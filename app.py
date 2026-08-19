90b850206daa1a44070d93c70d0190d97d4efe8b
malavneha <malavneha855@gmail.com>
2026-08-19 07:59:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ if question:
         )
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=prompt
         )
 
